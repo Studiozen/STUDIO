1845fd1922f763f4e1a60e4084d401d53419d088
Francesco Vizzi <francescovizzi636@gmail.com>
2025-11-20 22:08:51 +0000

    "Blocked cross-origin request" da che cosa deriva

--- next.config.ts
+++ next.config.ts
@@ -31,12 +31,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-      // Allow requests from the development environment
-      allowedDevOrigins: [
-          'https://6000-firebase-studio-1763653384760.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev',
-      ],
-  },
+  // Allow requests from the development environment
+  allowedDevOrigins: [
+      'https://6000-firebase-studio-1763653384760.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev',
+  ],
 };
 
 export default nextConfig;
