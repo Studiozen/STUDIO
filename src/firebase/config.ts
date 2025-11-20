ee68e9eb96fcae37195196e2f14899c68cb0e284
Francesco Vizzi <francescovizzi636@gmail.com>
2025-11-20 16:18:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-30473466-5d76c",
+  "appId": "1:795028137364:web:fcd47e740805c6ccd25b81",
+  "apiKey": "AIzaSyAP_hAHVvjvSUlEYvHuuj2CvYg2iC6YjZ0",
+  "authDomain": "studio-30473466-5d76c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "795028137364"
+};
