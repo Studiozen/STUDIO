6b55787a447004e5ed86cacd9e2702a9655ad7ec
Francesco Vizzi <francescovizzi636@gmail.com>
2025-11-20 21:30:27 +0000

    sistema ilproblema qunado ravvio il sito non va

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // allowedDevOrigins: [
-    //   'https://6000-firebase-studio-1763653384760.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev',
-    // ],
-  },
+  allowedDevOrigins: [
+    'https://6000-firebase-studio-1763653384760.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev',
+  ],
   typescript: {
     ignoreBuildErrors: true,
   },
