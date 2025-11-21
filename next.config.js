88553ff3216fc7e43ded9e5850ae87f1217f2af4
Francesco Vizzi <francescovizzi636@gmail.com>
2025-11-21 08:22:04 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    // This is the correct way to allow cross-origin requests in development
+    // for newer versions of Next.js, which solves the server restart loop.
+    allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+  },
 };
 
 module.exports = nextConfig;
