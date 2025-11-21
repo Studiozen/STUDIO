5c8e9fe1be7fce8b6aa17b3de70ccaa465cc6c5f
Francesco Vizzi <francescovizzi636@gmail.com>
2025-11-21 12:44:11 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
   },
   devIndicators: {
     allowedDevOrigins: [
-        '6000-firebase-studio-1763653384760.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev'
+        'https://6000-firebase-studio-1763653384760.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev'
     ],
   },
 };
